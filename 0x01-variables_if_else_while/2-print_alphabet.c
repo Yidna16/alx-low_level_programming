6b5a1efa02f8c080caaf78e607c266f49913a96b
yidnkachew <ydtamire16@gmail.com>
2023-02-18 09:43:45 -0800

    display alphabet using putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+* main-display alphabet
+* Return: always 0
+*/
 int main(void)
 {
-char s[] = {'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'};
+char s[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
 int i = 0;
 while (s[i] != '\n' && s[i] != '\0')
 {
