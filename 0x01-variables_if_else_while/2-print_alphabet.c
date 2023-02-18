ba74ce589caea3e814098977ce8dfaf5066b4579
yidnkachew <ydtamire16@gmail.com>
2023-02-18 09:29:15 -0800

    display alphabet using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+char s[] = {'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'};
+int i = 0;
+while (s[i] != '\n' && s[i] != '\0')
+{
+putchar(s[i]);
+i++;
+}
+putchar('\n');
+return (0);
+
+
+}
