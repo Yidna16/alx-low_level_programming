0ffdf0787ea55302f689b95ff41068908867e724
yidnkachew <ydtamire16@gmail.com>
2023-02-19 04:57:22 -0800

    display alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main-display alphabets reverse
+*Return: always 0
+*/
+int main(void)
+{
+int i;
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
