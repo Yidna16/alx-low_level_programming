04b21984a665a4ebae68bbfe5b6c6754750fdf90
yidnkachew <ydtamire16@gmail.com>
2023-02-23 12:27:30 -0800

    draws a straight line in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_line-prints line
+*Return:0
+*@n:input number
+*/
+void print_line(int n)
+{
+int i;
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
