0aac2d8f2b6667ae94b3eb47023756cdea858dda
yidnkachew <ydtamire16@gmail.com>
2023-02-25 01:58:50 -0800

    tell if -ve, +ve, 0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_sign-filter out -ve, +ve, 0
+*@n: input
+*Return: 0
+*/
+int print_sign(int n)
+{
+if (n > 0) 
+{
+_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+_putchar(45);
+return (-1);
+}
