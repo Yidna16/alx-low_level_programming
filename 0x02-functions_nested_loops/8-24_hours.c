1a853122872ccf6002a84dd82d9196fceb15c416
yidnkachew <ydtamire16@gmail.com>
2023-02-26 04:39:16 -0800

    print every second in a day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+* jack_bauer-print out every second in a day
+* Return: void
+*/
+void jack_bauer(void)
+{
+int i;
+int j;
+int k;
+int z;
+int hr2;
+
+for (i = 48; i < 51; i++)
+{
+if (i == 50)
+{
+hr2 = 51;
+}
+else if (i != 50)
+{
+hr2 = 57;
+}
+for (j = 48; j <= hr2; j++)
+{
+for (k = 48; k < 54; k++)
+{
+for (z = 48; z <= 57; z++)
+{
+_putchar(i);
+_putchar(j);
+_putchar(':');
+_putchar(k);
+_putchar(z);
+_putchar('\n');
+}
+}
+}
+}
+
+}
