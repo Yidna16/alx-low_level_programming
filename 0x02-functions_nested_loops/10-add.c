a1d8eec0e6fa7e6c739007e90f976e67a00fb17b
yidnkachew <ydtamire16@gmail.com>
2023-02-26 05:02:23 -0800

    adding programe

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: Integers to add
+ * @b: Integer to add
+ * Return: Addition integers
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
