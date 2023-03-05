763153452eabb2f1ba06ecda7807f1d84bf4af48
yidnkachew <ydtamire16@gmail.com>
2023-03-05 07:07:55 -0800

    change to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+/**
+ * string_toupper-change to upper case
+ * Return:char value
+*/
+char *string_toupper(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 97 && s[i] <= 122)
+{
+s[i] -= 32;
+}
+}
+return (s);
+}
