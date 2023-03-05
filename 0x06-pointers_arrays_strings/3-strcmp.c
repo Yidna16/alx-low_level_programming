0639b30e408eee0c2d3a6ee1dd12c6fec6b7a445
yidnkachew <ydtamire16@gmail.com>
2023-03-05 05:20:43 -0800

    comparizen

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,18 +7,10 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-if (*s1 == *s2)
+int i = 0;
+while (s1[i] == s2[i] && s1[i] != '\0')
 {
-i = 0;
+i++;
 }
-else if (*s1 > *s2)
-{
-i = 15;
-}
-else
-{
-i = -15;
-}
-return (i);
+return (s1[i]-s2[i]);
 }
