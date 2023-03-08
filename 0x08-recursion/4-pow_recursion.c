c80f38c9c1b4afa152d893f17b679a10232df74b
yidnkachew <ydtamire16@gmail.com>
2023-03-08 06:26:36 -0800

    exponeshal function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+/**
+ *_pow_recursion-return x the power of y
+ *@x:input
+ *@y:input
+ *Return:int value
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
