6bf91f3bd3dfb9240f155cfd99e38b17046360b2
yidnkachew <ydtamire16@gmail.com>
2023-03-14 06:19:09 -0700

    concatenat using malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+/**
+ * str_concat-return concatenat one
+ * @s1:destination
+ * @s2:source
+ * Return:s1 value
+*/
+char *str_concat(char *s1, char *s2)
+{
+char *duplicate_str;
+int i = 0, len = 0, j = 0, k = 0;
+
+while (*(s1 + j))
+i++, j++;
+while (*(s2 + k))
+len++, k++;
+
+duplicate_str = malloc(sizeof(char) * (i + len));
+
+if (duplicate_str == NULL)
+return (NULL);
+
+j = 0, k = 0;
+while (j < i)
+{
+*(duplicate_str + j) = *(s1 + j);
+j++;
+}
+while (k < len)
+{
+*(duplicate_str + j) = *(s2 + k);
+j++;
+k++;
+}
+return (duplicate_str);
+}
