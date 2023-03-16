4adef78609b6fc7b0a70774e5fc95af3161ef34b
yidnkachew <ydtamire16@gmail.com>
2023-03-16 10:29:15 -0700

    allocate with value from min to max

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+/**
+ * array_range-hold the value from min to max
+ * @min:min value
+ * @max:max value
+ * Return:pointer to the new array
+*/
+int *array_range(int min, int max)
+{
+int *p, i = 0;
+if (min > max)
+{
+return (NULL);
+}
+p = malloc((max - min) * sizeof(int));
+if (p == NULL)
+{
+return (NULL);
+}
+for (; min < max; min++)
+{
+p[i] = min;
+i++;
+}
+return (p);
+}
