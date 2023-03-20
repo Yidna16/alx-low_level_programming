6a8e28715eb245b1e51917204b152e3dcd89b5e7
yidnkachew <ydtamire16@gmail.com>
2023-03-20 08:18:47 -0700

    initialize struct dog variabel

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog-holds differnts data types about a dog info
  * @name:name of the dog
@@ -11,4 +12,5 @@ struct dog
 float age;
 char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif/*DOG_H*/
