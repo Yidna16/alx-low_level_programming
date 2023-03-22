dd8bd8e670b7e61171c38e8964c9b5b0fc4528ac
yidnkachew <ydtamire16@gmail.com>
2023-03-22 07:10:48 -0700

    search for integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+/**
+ * int_index-search an integer
+ * @array:array
+ * @size:size of the array
+ * @cmp:function pointer
+ * Return:int value
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i = 0, k = 0;
+if (size <= 0)
+{
+return (-1);
+}
+while (i < size)
+{
+if (cmp(array[i]))
+{
+return (i);
+}
+i++;
+}
+return (k);
+}
