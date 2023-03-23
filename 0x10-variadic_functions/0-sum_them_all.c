742bd52f1d6a0b449904ed2fdcee4bcfe7c2698f
yidnkachew <ydtamire16@gmail.com>
2023-03-23 06:23:36 -0700

    fixed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,9 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 /**
  * sum_them_all-sum up all it's arguments
  * @n:first unchanged argument
- * 
+ * @...:variabel argument 
  * return:the sum value
 */
 int sum_them_all(const unsigned int n, ...)
