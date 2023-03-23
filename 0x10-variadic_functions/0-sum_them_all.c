1b5143908d68010d8e57ce126966ba239615c5cb
yidnkachew <ydtamire16@gmail.com>
2023-03-23 07:32:30 -0700

    fixed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,13 +3,14 @@
 /**
  * sum_them_all-sum up all it's arguments
  * @n:first unchanged argument
- * @...:variabel argument 
- * return:the sum value
+ *
+ * Return:the sum value
 */
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int i, x = 0;
 va_list d;
+unsigned int i;
+int x = 0;
 if (n == 0)
 {
 return (0);
