541790fb855574be401c151a74b8399de9e71708
yidnkachew <ydtamire16@gmail.com>
2023-04-04 07:52:22 -0700

    free the list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,10 @@ void free_listint2(listint_t **head)
 listint_t *p;
 listint_t *i;
 p = *head;
+*head = NULL;
 while (p != NULL)
 {
 i = p->next;
-if (p != *head)
-{
-*head = NULL;
-}
 free(p);
 p = i;
 }
