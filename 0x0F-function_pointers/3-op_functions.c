227a571db006e6dce0e26f374993b6bf0c0c8494
yidna <ydtamire16@gmail.com>
2023-04-26 08:25:27 -0700

    simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,19 +1,49 @@
+/**
+ * op_add-add a & b
+ * @a:parameter
+ * @b:parameter
+ * Return:sum
+*/
 int op_add(int a, int b)
 {
 return (a + b);
 }
+/**
+ * op_sub-substracte a & b
+ * @a:parameter
+ * @b:parameter
+ * Return:substracted
+*/
 int op_sub(int a, int b)
 {
 return (a - b);
 }
+/**
+ * op_mul-multiply a & b
+ * @a:parameter
+ * @b:parameter
+ * Return:multiplication
+*/
 int op_mul(int a, int b)
 {
 return (a * b);
 }
+/**
+ * op_div-divide a & b
+ * @a:parameter
+ * @b:parameter
+ * Return:division
+*/
 int op_div(int a, int b)
 {
 return (a / b);
 }
+/**
+ * op_mod-remainder of a & b
+ * @a:parameter
+ * @b:parameter
+ * Return:remander
+*/
 int op_mod(int a, int b)
 {
 return (a % b);
