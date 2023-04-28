c87be1c01173d4580ba5b8e4a0ffa56b4d86ee06
yidna <ydtamire16@gmail.com>
2023-04-28 02:11:18 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,13 +11,13 @@ size_t print_list(const list_t *h)
 size_t i = 0;
 while (h != NULL)
 {
-if (h->str != NULL)
+if (h->str == NULL)
 {
-printf("[%d] %s\n", h->len, h->str);
+printf("[0] (nil)\n");
 }
 else
 {
-printf("[0] (nil)\n");
+printf("[%d] %s\n", h->len, h->str);
 }
 i++;
 h = h->next;
