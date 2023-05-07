0ed95cff390b37531c51a87c7df21f61e468ea8e
yidna <ydtamire16@gmail.com>
2023-05-07 10:35:13 -0700

    get endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+/**
+ * get_endianness-a function that checks the endianness
+ * Return:0 if big endian, 1 if little endian
+*/
+int get_endianness(void)
+{
+int i = 1;
+if (*((char *)&i) == 1)
+return (1);
+else
+return (0);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
  * @y:input
  * Return:powered value
 */
+int get_endianness(void);
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
